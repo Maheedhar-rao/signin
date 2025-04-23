dbc08e3f09a466bba77a33129fbf2300c156326a
Maheedhar rao Govada <106291633+Maheedhar-rao@users.noreply.github.com>
2025-04-23 14:11:05 -0400

    Create index.js
    initial commit for index.js file

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/auth', authRoutes);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Signin service running on http://localhost:${PORT}`);
+});
