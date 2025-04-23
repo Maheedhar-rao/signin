aa24cb14998734007f8e325f7c854492e79c7319
Maheedhar rao Govada <106291633+Maheedhar-rao@users.noreply.github.com>
2025-04-23 17:37:22 -0400

    Update sendEmail.js
    CHANGED THE VARIABLE NAME

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,14 +3,14 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_FROM,
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
 
 async function sendEmail(to, message) {
   await transporter.sendMail({
-    from: `"Your CRM App" <${process.env.EMAIL_FROM}>`,
+    from: `"CROC CRM - " <${process.env.EMAIL_FROM}>`,
     to,
     subject: 'Your Login Verification Code',
     text: message
