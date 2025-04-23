9c0f0e0a794e08278aa22416a3eeca9e1149e36d
Maheedhar rao Govada <106291633+Maheedhar-rao@users.noreply.github.com>
2025-04-23 17:24:22 -0400

    Create sendEmail.js
    initial commit for sendEmail.js

---
+++ server/utils/sendEmail.js
@@ -0,0 +1,20 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_FROM,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+async function sendEmail(to, message) {
+  await transporter.sendMail({
+    from: `"Your CRM App" <${process.env.EMAIL_FROM}>`,
+    to,
+    subject: 'Your Login Verification Code',
+    text: message
+  });
+}
+
+module.exports = sendEmail;
